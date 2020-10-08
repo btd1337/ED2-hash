bb96f1daad4b51f8a2e7ec6b781bedd56b5b1247
Helder Bertoldo <helder.bertoldo@haizen.tech>
2020-10-07 23:02:57 -0300

    Implements division and multiply

--- src/hash/Hash.java
+++ src/hash/Hash.java
@@ -1,12 +1,21 @@
 package hash;
 
 import java.util.ArrayList;
+import java.util.function.BiFunction;
 
 public class Hash {
 	ArrayList<Pair>[] hashTable;
+	int tableSize;
+	BiFunction<Integer, Integer,Integer> hash;
+	
+	static BiFunction <Integer, Integer, Integer> division = (index, tableSize) -> index % tableSize;
+	static BiFunction <Integer, Integer, Double> multiplication = (index, tableSize) -> Math.floor(tableSize*(index*((Math.sqrt(5)-1)/2)%1));
 	
 	@SuppressWarnings("unchecked")
-	void create(int tableSize) {
+	void create(int tableSize, BiFunction<Integer, Integer,Integer> hash) {
+		this.tableSize = tableSize;
+		this.hash = hash;
+
 		this.hashTable = new ArrayList[tableSize];
 		
 		for(int i=0; i<tableSize; i++) {
@@ -15,13 +24,33 @@ public class Hash {
 	}
 	
 	Pair insert(int key, String data) {
-		this.hashTable[0].add(new Pair(key, data));
-		int lastElementIndex = this.hashTable[0].size() - 1;
-		return this.hashTable[0].get(lastElementIndex);
+		int tableIndex = this.getHashTableIndex(key);
+		this.hashTable[tableIndex].add(new Pair(key, data));
+		int lastElementIndex = this.hashTable[tableIndex].size() - 1;
+		return this.hashTable[tableIndex].get(lastElementIndex);
 	}
 	
 	Pair lookup(int key, String data) {
-		int index = this.hashTable[0].indexOf(new Pair(key, data));
-		return index != -1 ? this.hashTable[0].get(index) : null;
+		int tableIndex = this.getHashTableIndex(key);
+		int index = this.hashTable[tableIndex].indexOf(new Pair(key, data));
+		return index != -1 ? this.hashTable[tableIndex].get(index) : null;
+	}
+	
+	void detroy() {
+		for(int i=0; i < this.hashTable.length; i++) {
+			this.hashTable[i].clear();
+		}
+	}
+	
+	int getHashTableIndex(int key) {
+		return this.hash.apply(key, this.tableSize);
+	}
+	
+	void printHash() {
+		for(int i=0; i<this.tableSize; i++) {
+			for(int j=0; j < this.hashTable[i].size(); j++) {
+				System.out.println("[" + i + ", " + j + "] = " + this.hashTable[i].get(j));							
+			}
+		}
 	}
 }
--- src/hash/Main.java
+++ src/hash/Main.java
@@ -3,10 +3,12 @@ package hash;
 public class Main {
 
 	public static void main(String[] args) {
+		// System.out.println(4 % 5);
 		Hash hash = new Hash();
-		hash.create(5);
-		hash.insert(1, "Brasil");
-		System.out.println(hash.lookup(1, "brasil"));
+		hash.create(5, Hash.division);
+		hash.insert(1, "Brasil1");
+		
+		hash.printHash();
 	}
 
 }
